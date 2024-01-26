6293c40604d7a7034c7de74b2a2ff379108b6fad
Dmytro <dmytro@MacBook-Pro-Dmytro.local>
2024-01-26 20:38:22 +0200

    changed to firebase

--- app/src/main/java/com/fernfog/pricelist/DownloadActivity.java
+++ app/src/main/java/com/fernfog/pricelist/DownloadActivity.java
@@ -1,22 +1,22 @@
 package com.fernfog.pricelist;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.AsyncTask;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputLayout;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
-import java.net.MalformedURLException;
-import java.net.UnknownHostException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class DownloadActivity extends AppCompatActivity {
+
+    FirebaseStorage storage = FirebaseStorage.getInstance("gs://somephingg.appspot.com");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,51 +31,22 @@ public class DownloadActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String link = textInputLayout.getEditText().getText().toString().trim();
                 if (!link.isEmpty()) {
-                    new DownloadFileTask().execute(link);
                     materialButton.setActivated(false);
+                    StorageReference storageRef = storage.getReference();
+
+                    storageRef.child(link).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            FileDownloader fileDownloader = new FileDownloader();
+                            fileDownloader.downloadFile(getApplicationContext(), uri.toString(), "price.xlsm");
+                        }
+                    });
+
                 } else {
                     Toast.makeText(DownloadActivity.this, getString(R.string.toastLinkEmptyText), Toast.LENGTH_LONG).show();
                     materialButton.setEnabled(true);
                 }
             }
         });
-
-
-
-
     }
-
-    private class DownloadFileTask extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... params) {
-            String fileUrl = params[0];
-
-            try {
-                Document document = Jsoup.connect(fileUrl).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64)").get();
-
-                FileDownloader fileDownloader = new FileDownloader();
-                fileDownloader.downloadFile(getApplicationContext(), document.select("a.download-link").first().attr("href"), "price.xlsm");
-
-                return getString(R.string.toastDownloadSuccesful);
-
-            } catch (MalformedURLException e) {
-                return getString(R.string.toastWrongLinkText);
-            } catch (UnknownHostException e) {
-                return getString(R.string.toastUnkownOrNoInternetText);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            if (result != null) {
-                Toast.makeText(DownloadActivity.this, result, Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-
 }
